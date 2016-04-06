ecfcf3fd35ebd51149bbbd053972b5e0f6cbd9dd
telegrambot81 <telegrambot81@gmail.com>
2016-04-06 15:19:38 +0430

    Update gm.lua

--- plugins/gm.lua
+++ plugins/gm.lua
@@ -5,7 +5,7 @@ do
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
     if not is_admin(msg) then
-        return "This service is private for SUDO (@shayansoft)"
+        return "This service is private for SUDO (@Oo_mehdi_shar_khoffash_revale_oO)"
     end
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
